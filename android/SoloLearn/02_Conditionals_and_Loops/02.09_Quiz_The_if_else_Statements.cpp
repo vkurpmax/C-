e5570b204529352810970bed1f118fbcb7052f40
Putu Herdy <45077089+vkurpmax@users.noreply.github.com>
2019-05-27 16:45:38 +0800

    Create 02.09_Quiz_The_if_else_Statements.cpp

---
+++ android/SoloLearn/02_Conditionals_and_Loops/02.09_Quiz_The_if_else_Statements.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x = 10;
+	int y = 20;
+	if (x > y) {
+		cout << "some other text" << endl;
+	}
+	else {
+		cout << "printing in the else" << endl;
+	}
+}
+
+// Quiz The if else Statement.
+// Fill in the blanks to compare the variables and print the "printing in the else" text to the screen:
