d1d67abf2cdbc5ec1beab83fcdd8fef771b0502a
Putu Herdy <45077089+vkurpmax@users.noreply.github.com>
2019-05-27 15:31:53 +0800

    Create 01.06_Quiz.cpp

---
+++ android/SoloLearn/02_Conditionals_and_Loops/01.06_Quiz.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 98;
+	int b = 76;
+	if (a > b) {
+		cout << "greatest is " << a << endl;
+	}
+	if (b > a) {
+		cout << "greatest is " << b << endl;
+	}
+	return 0;
+}
