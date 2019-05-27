3757d217f2a8dcf6787197590b72326a895e5e2f
Putu Herdy <45077089+vkurpmax@users.noreply.github.com>
2019-05-27 15:28:42 +0800

    Create 01.05_Relational_Operators.cpp

---
+++ android/SoloLearn/02_Conditionals_and_Loops/01.05_Relational_Operators.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 55;
+	int b = 33;
+	if (a > b) {
+		cout << "a is greates than b";
+	}
+	// Outputs "a is greates than b"
+	return 0;
+}
