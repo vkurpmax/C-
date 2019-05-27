9d82b7affb952130c297b4b3eaf2f4a18aebb372
Putu Herdy <45077089+vkurpmax@users.noreply.github.com>
2019-05-27 16:35:15 +0800

    Create 02.07_Nested_if_else_Statements.cpp

---
+++ android/SoloLearn/02_Conditionals_and_Loops/02.07_Nested_if_else_Statements.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int age = 18;
+	if (age > 14) {
+		if (age >= 18) {
+			cout << "Adult";
+		}
+		else {
+			cout << "Teenager";
+		}
+	}
+	else {
+		if (age > 0) {
+			cout << "Child";
+		}
+		else {
+			cout << "Something's wrong";
+		}
+	}
+}
+
+// C++ provides the option of nesting an unlimited number of if/else statements.
