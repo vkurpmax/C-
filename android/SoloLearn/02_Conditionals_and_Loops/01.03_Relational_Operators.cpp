16511c99e33bef10cee8068bec04f446fec3b406
Putu Herdy <45077089+vkurpmax@users.noreply.github.com>
2019-05-27 14:59:16 +0800

    Create 01.03_Relational_Operators.cpp

---
+++ android/SoloLearn/02_Conditionals_and_Loops/01.03_Relational_Operators.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	if (10 != 10) {
+		cout << "Yes";
+	}
+	// The Above condition evaluates to "False" and the block of code is not executed
+	return 0;
+}
