f79025bf55f3b7847948e1696f8ba18d0bb56d27
Putu Herdy <45077089+vkurpmax@users.noreply.github.com>
2019-05-27 16:40:39 +0800

    Create 02.08_The_if_else_Statements.cpp

---
+++ android/SoloLearn/02_Conditionals_and_Loops/02.08_The_if_else_Statements.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 10;
+	if (a > 4)
+		cout << "Yes";
+	else
+		cout << "No";
+}
+
+// The if else Statement.
+// In if/else statements, "a single statement can be included without enclosing it into curly braces.
