2fa85934aaaafa76c0e49197847102997ba29d9d
Putu Herdy <45077089+vkurpmax@users.noreply.github.com>
2019-05-27 16:06:24 +0800

    Create 02.02_The_Else_Statement.cpp

---
+++ android/SoloLearn/02_Conditionals_and_Loops/02.02_The_Else_Statement.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int mark = 90;
+
+	if (mark < 50) {
+		cout << "You failed." << endl;
+	}
+	else {
+		cout << "You passed." << endl;
+	}
+	// Outputs "You passed."
+}
