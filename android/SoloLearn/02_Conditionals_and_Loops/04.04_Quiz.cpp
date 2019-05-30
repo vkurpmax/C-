15e2799aec6f78a92f458ef154b2a61cb6b41d1b
Putu Herdy <45077089+vkurpmax@users.noreply.github.com>
2019-05-30 16:30:02 +0800

    Create 04.04_Quiz.cpp

---
+++ android/SoloLearn/02_Conditionals_and_Loops/04.04_Quiz.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x = 1;
+	int number;
+	while (x <= 5)
+	{
+		cin >> number;
+		x++;
+	}
+}
+/*QUIZ
+Fill in blanks to allow user to enter a value using cin and store it in the 'number' variable 5 times.*/
