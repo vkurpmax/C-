9176adc0ff80752bf7c7f1ab2764c1ab73abdd40
Putu Herdy <45077089+vkurpmax@users.noreply.github.com>
2019-05-30 16:20:28 +0800

    Create 04.02_Quiz.cpp

---
+++ android/SoloLearn/02_Conditionals_and_Loops/04.02_Quiz.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x = 1;
+	while (x <= 5)
+	{
+		cout << x << endl;
+		x++;
+	}
+}
+/*QUIZ
+Fill in the blanks to print x's values from 1 to 5. Increment x's values using ++ ooperator.*/
