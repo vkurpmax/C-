a0bd654b239654a385f6b9ef0898e8b76f3755eb
Putu Herdy <45077089+vkurpmax@users.noreply.github.com>
2019-05-30 21:21:35 +0800

    Create 05.02_Quiz_The_For_Loop.cpp

---
+++ android/SoloLearn/02_Conditionals_and_Loops/05.02_Quiz_The_For_Loop.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	for (int x = 1; x <= 10; x++)
+	{
+		cout << x << endl;
+	}
+}
+/*QUIZ
+Fill in the blanks to print x's values to the screen 10 times:*/
