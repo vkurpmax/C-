9cd92724e11dbc507680a22c71ca3164aa766ff4
Putu Herdy <45077089+vkurpmax@users.noreply.github.com>
2019-05-30 21:37:43 +0800

    Create 05.04_Quiz_The_For_Loop.cpp

---
+++ android/SoloLearn/02_Conditionals_and_Loops/05.04_Quiz_The_For_Loop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	for (int x = 0; x <= 20; x += 2)
+	{
+		cout << x << endl;
+	}
+}
+/*QUIZ
+Fill in the blanks to print EVEN
+(multiples of 2) values from 0 to 20 using a for loop:*/
