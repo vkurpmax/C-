210da9afd4ab01417683bc34f010414b5f99407d
Putu Herdy <45077089+vkurpmax@users.noreply.github.com>
2019-05-30 16:26:52 +0800

    Create 04.03_Using_a_While_Loop.cpp

---
+++ android/SoloLearn/02_Conditionals_and_Loops/04.03_Using_a_While_Loop.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num = 1;
+	int number;
+
+	while (num <= 5)
+	{
+		cin >> number;
+		num++;
+	}
+}
+/*USING A WHILE LOOP
+
+A loop can be used to obtain multiple inputs from the user.
+Let's create a program that allows the user to enter a number 5 times, each time storing the input in a variable.\
+
+The above code asks for user input 5 times, and each time saves the input in the number variable.*/
