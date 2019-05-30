24393ed757af329fd77110854e3e9e99f70599ea
Putu Herdy <45077089+vkurpmax@users.noreply.github.com>
2019-05-30 16:15:00 +0800

    Create 04.01_Using_Increment_or_Decrement.cpp

---
+++ android/SoloLearn/02_Conditionals_and_Loops/04.01_Using_Increment_or_Decrement.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num = 1;
+	while (num < 6)
+	{
+		cout << "Number: " << num << endl;
+		num++;
+	}
+
+	/* Outputs
+	Number: 1
+	Number: 2
+	Number: 3
+	Number: 4
+	Number: 5
+	*/
+}
