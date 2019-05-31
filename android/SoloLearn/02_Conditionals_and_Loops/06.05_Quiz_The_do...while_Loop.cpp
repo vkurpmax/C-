e57be2a26e34141a863e514b9ad1f730c981b66b
Putu Herdy <45077089+vkurpmax@users.noreply.github.com>
2019-06-01 04:25:16 +0800

    Create 06.05_Quiz_The_do...while_Loop.cpp

---
+++ android/SoloLearn/02_Conditionals_and_Loops/06.05_Quiz_The_do...while_Loop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x = 1;
+	do {
+		cout << "this is a loop" << endl;
+		x++;
+	} 	
+	while (x <= 15);
+}
+/*QUIZ
+Fill in the blanks to print "this is a loop" to the screen 15 times.*/
