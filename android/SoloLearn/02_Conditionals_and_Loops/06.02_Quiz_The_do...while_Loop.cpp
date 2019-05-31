9339f422846108406c6956d68664522ff19e8c48
Putu Herdy <45077089+vkurpmax@users.noreply.github.com>
2019-06-01 04:12:22 +0800

    Create 06.02_Quiz_The_do...while_Loop.cpp

---
+++ android/SoloLearn/02_Conditionals_and_Loops/06.02_Quiz_The_do...while_Loop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x = 0;
+	do {
+		cout << x << endl;
+		x++;
+	}
+	while (x < 10);
+}
+/*QUIZ
+Fill in the blanks to print x variable's values 10 times.*/
