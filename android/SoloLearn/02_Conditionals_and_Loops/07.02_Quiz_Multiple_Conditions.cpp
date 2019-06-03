75676ed0e5159af7743dbf28512680ac8a21df47
Putu Herdy <45077089+vkurpmax@users.noreply.github.com>
2019-06-03 12:55:31 +0800

    Create 07.02_Quiz_Multiple_Conditions.cpp

---
+++ android/SoloLearn/02_Conditionals_and_Loops/07.02_Quiz_Multiple_Conditions.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int age;
+	cin >> age;
+	if (age == 16) {
+		cout << "you can drive a car" << endl;
+	}
+	if (age == 18) {
+		cout << "buy a lottery ticket" << endl;
+	}
+}
+/*QUIZ
+Fill in the blanks to print "you can drive a car" if the variable 'age' equals 16, print "buy a lottery ticket"
+if the 'age' is equals to 18.*/
