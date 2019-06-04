f14bbe2b082043cd40c465b76b30ba2d66862641
Putu Herdy <45077089+vkurpmax@users.noreply.github.com>
2019-06-05 01:33:16 +0800

    Create 08.05_Quiz_The_OR_Operator.cpp

---
+++ android/SoloLearn/02_Conditionals_and_Loops/08.05_Quiz_The_OR_Operator.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int age = 26;
+	int money = 4;
+
+	if (age > 21 || money > 500) {
+		cout << "welcome" << endl;
+	}
+}
+/*QUIZ
+Fill in the blanks to print "welcome" on the screen.*/
