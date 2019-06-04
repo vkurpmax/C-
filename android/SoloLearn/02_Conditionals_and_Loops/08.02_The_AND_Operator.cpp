0fe25cccf4a725cf99d0c8ab5c8c0ddd341a01d4
Putu Herdy <45077089+vkurpmax@users.noreply.github.com>
2019-06-05 01:18:30 +0800

    Create 08.02_The_AND_Operator.cpp

---
+++ android/SoloLearn/02_Conditionals_and_Loops/08.02_The_AND_Operator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int age = 20;
+	if (age > 16 && age < 60) {
+		cout << "Accepted" << endl;
+	}
+	/*Outputs "Accepted"*/
+}
+/*THE AND OPERATOR
+In the example above, the logical AND operator was used to combine both expressions.
+
+The expression in the if statement evaluates to "true" only if both expressions are "true".*/
