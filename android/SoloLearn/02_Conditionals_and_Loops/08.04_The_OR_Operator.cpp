ef8ff69c5af2f8d4499c17db140f4d46c5627650
Putu Herdy <45077089+vkurpmax@users.noreply.github.com>
2019-06-05 01:30:22 +0800

    Create 08.04_The_OR_Operator.cpp

---
+++ android/SoloLearn/02_Conditionals_and_Loops/08.04_The_OR_Operator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int age = 16;
+	int score = 90;
+
+	if (age > 20 || score > 50) {
+		cout << "Accepted" << endl;
+	}
+	/* Outputs "Accepted"*/
+}
+/*THE OR OPERATOR
+The OR (||) operator returns true if any one of its operands is "true".
+
+You can combine any number or logical "OR" statements you want.
+In addition, multiple "OR" and "AND" statements may be claimed together.*/
