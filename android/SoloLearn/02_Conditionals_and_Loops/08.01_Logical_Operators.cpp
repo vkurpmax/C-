305a83a5c31cad649752ee33f861b41c2502a9ad
Putu Herdy <45077089+vkurpmax@users.noreply.github.com>
2019-06-05 01:13:45 +0800

    Create 08.01_Logical_Operators.cpp

---
+++ android/SoloLearn/02_Conditionals_and_Loops/08.01_Logical_Operators.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int age = 23;
+	int money = 4;
+	if (age > 21 && money > 500)	{
+		cout << "welcome" << endl;
+	}
+}
+/*LOGICAL OPERATORS
+Use "logical operators" to combine conditional statements and return "true" or "false".
+&& AND operator
+|| OR operator
+! NOT operator
+
+In the AND operator, both operands must be "true" for the entire expression to be "true".*/
+
+/*QUIZ
+Fill in the blanks to test both conditions in the following if statement.*/
