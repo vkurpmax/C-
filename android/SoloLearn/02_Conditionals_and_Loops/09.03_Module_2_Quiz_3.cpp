3e9e48615bb24bf65da7f9ffdd96b7353de52189
Putu Herdy <45077089+vkurpmax@users.noreply.github.com>
2019-06-08 17:28:12 +0800

    Create 09.03_Module_2_Quiz_3.cpp

---
+++ android/SoloLearn/02_Conditionals_and_Loops/09.03_Module_2_Quiz_3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	for (int x = 0; x < 7; x++) {
+		cout << "in a loop" << endl;
+	}
+}
+/*Module 2 Quiz
+
+3
+Type in a code to print "in a loop" to the screen 7 times using the for loop*/
