fa057c5c3714e81d46cf05cc3eb50d17092140fa
Putu Herdy <45077089+vkurpmax@users.noreply.github.com>
2019-06-08 17:24:33 +0800

    Create 09.02_Module_2_Quiz_2.cpp

---
+++ android/SoloLearn/02_Conditionals_and_Loops/09.02_Module_2_Quiz_2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x = 1;
+	while (x <= 5) {
+		cout << "in a loop" << endl;
+		x++;
+	}
+}
+/*Module 2 Quiz
+
+2
+Type in a code to print "in a loop" to the screen 5 times using the while loop.*/
