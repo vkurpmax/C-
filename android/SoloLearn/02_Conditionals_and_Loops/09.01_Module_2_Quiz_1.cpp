4456d33951b94dc6dcc7f3f71f7ec8e84ad7efbc
Putu Herdy <45077089+vkurpmax@users.noreply.github.com>
2019-06-08 17:21:08 +0800

    Create 09.01_Module_2_Quiz_1.cpp

---
+++ android/SoloLearn/02_Conditionals_and_Loops/09.01_Module_2_Quiz_1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x;
+	cin >> x;
+	if (x > 20) {
+		cout << "x is big" << endl;
+	}
+	if (x < 10) {
+		cout << "x is small" << endl;
+	}
+}
+/*Module 2 Quiz
+
+1
+Type in the code to enter a value and store in the variable x,
+then test if it's greater than 20,
+print "x is big" on the screen,
+if x is less than 10,
+print "x is small"*/
