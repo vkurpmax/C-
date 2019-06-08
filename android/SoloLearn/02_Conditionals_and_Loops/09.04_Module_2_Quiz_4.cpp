ad7573e8a365debf611761d61fa7134f50b56eb1
Putu Herdy <45077089+vkurpmax@users.noreply.github.com>
2019-06-08 17:32:46 +0800

    Create 09.04_Module_2_Quiz_4.cpp

---
+++ android/SoloLearn/02_Conditionals_and_Loops/09.04_Module_2_Quiz_4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 144;
+	int b = 33;
+	if (a > 12 && b <= 76) {
+		cout << "You rock!" << endl;
+	}
+}
+/*Module 2 Quiz
+
+4
+Fill in the blanks to print "You rock!" to the screen if variable a is greater than 12,
+and variable b is less than or equal to 76.*/
